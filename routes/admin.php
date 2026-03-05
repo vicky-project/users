35003cec7d4053618dfedc093808ebddf1025577
vicky <pratamavr@gmail.com>
2026-03-06 01:45:06 +0800

    updates

--- routes/admin.php
+++ routes/admin.php
@@ -7,29 +7,29 @@ use Modules\Users\Http\Controllers\Admin\RoleController;
 use Modules\Users\Http\Controllers\Admin\UserController;
 
 Route::group(
-	["prefix" => "admin", "middleware" => "auth", "as" => "admin."],
-	function () {
-		Route::get("dashboard", [DashboardController::class, "index"])->name(
-			"dashboard",
-		);
+  ["middleware" => "auth",],
+  function () {
+    Route::get("dashboard", [DashboardController::class, "index"])->name(
+      "dashboard",
+    );
 
-		// Users
-		Route::get("users/{user}/assign-roles", [
-			UserController::class,
-			"assignRoles",
-		])->name("assign-roles");
-		Route::post("users/{user}/assign-roles", [
-			UserController::class,
-			"updateRoles",
-		])->name("assign-roles.update");
-		Route::resource("users", UserController::class);
+    // Users
+    Route::get("users/{user}/assign-roles", [
+      UserController::class,
+      "assignRoles",
+    ])->name("assign-roles");
+    Route::post("users/{user}/assign-roles", [
+      UserController::class,
+      "updateRoles",
+    ])->name("assign-roles.update");
+    Route::resource("users", UserController::class);
 
-		// Roles
-		Route::get('roles/{role}/assign-permissions',[RoleController::class,'assignPermissions'])->name('assign-permissions');
-		Route::post('roles/{role}/assign-permissions',[RoleController::class,'updatePermissions'])->name('assign-permissions.update');
-		Route::resource("roles", RoleController::class);
+    // Roles
+    Route::get('roles/{role}/assign-permissions', [RoleController::class, 'assignPermissions'])->name('assign-permissions');
+    Route::post('roles/{role}/assign-permissions', [RoleController::class, 'updatePermissions'])->name('assign-permissions.update');
+    Route::resource("roles", RoleController::class);
 
-		// Permissions
-		Route::resource("permissions", PermissionController::class);
-	},
+    // Permissions
+    Route::resource("permissions", PermissionController::class);
+  },
 );
