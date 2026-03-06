171480de7f5c54bb72f86feb2d057ad983908a4e
vicky <pratamavr@gmail.com>
2026-03-06 21:08:38 +0800

    updates

---
+++ app/Providers/Menu/UsersMenuProvider.php
@@ -0,0 +1,59 @@
+<?php
+namespace Modules\Users\Providers\Menu;
+
+use Modules\Users\Constants\Permission;
+use Modules\CoreUI\Services\BaseMenuProvider;
+
+class UsersMenuProvider extends BaseMenuProvider
+{
+  protected array $config = [
+    "group" => "server",
+    "location" => "sidebar",
+    "icon" => "bi bi-server",
+    "order" => 2,
+    "permission" => null,
+  ];
+
+  public function __construct() {
+    $moduleName = "Users";
+    parent::__construct($moduleName);
+  }
+
+  /**
+  * Get all menus
+  */
+  public function getMenus(): array
+  {
+    return [
+      $this->item([
+        "title" => "User Management",
+        "icon" => "bi bi-people",
+        "type" => "dropdown",
+        "order" => 50,
+        "children" => [
+          $this->item([
+            "title" => "Users",
+            "icon" => "bi bi-person-gear",
+            "route" => "admin.users.index",
+            "order" => 1,
+            "permission" => Permissions::VIEW_USERS,
+          ]),
+          $this->item([
+            "title" => "Roles",
+            "icon" => "bi bi-person-check",
+            "route" => "admin.roles.index",
+            "order" => 2,
+            "permission" => Permissions::VIEW_ROLES,
+          ]),
+          $this->item([
+            "title" => "Permissions",
+            "icon" => "bi bi-person-lock",
+            "route" => "admin.permissions.index",
+            "order" => 3,
+            "permission" => Permissions::VIEW_PERMISSIONS,
+          ]),
+        ],
+      ]),
+    ];
+  }
+}
