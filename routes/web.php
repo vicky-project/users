248356dfbdfafa5dbf31f3de7319a80eeb525a61
vicky <pratamavr@gmail.com>
2026-03-08 04:44:45 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -7,39 +7,45 @@ use Modules\Users\Http\Controllers\Auth\RegisterController;
 use Modules\Users\Http\Controllers\Auth\ResetPasswordController;
 use Modules\Users\Http\Controllers\UsersController;
 
-// Login
-Route::get("login", [LoginController::class, "showLoginForm"])->name("login");
-Route::post("login", [LoginController::class, "login"]);
-Route::post("logout", [LoginController::class, "logout"])->name("logout");
+// Authentication
+Route::middleware(['web', 'guest'])->group(function() {
+  // Login
+  Route::get("login", [LoginController::class, "showLoginForm"])->name("login");
+  Route::post("login", [LoginController::class, "login"]);
 
-// Register
-Route::get("register", [
-  RegisterController::class,
-  "showRegistrationForm",
-])->name("register");
-Route::post("register", [RegisterController::class, "register"]);
+  // Register
+  Route::get("register", [
+    RegisterController::class,
+    "showRegistrationForm",
+  ])->name("register");
+  Route::post("register", [RegisterController::class, "register"]);
+  // Forgot Password
+  Route::get("password/reset", [
+    ForgotPasswordController::class,
+    "showLinkRequestForm",
+  ])->name("password.request");
+  Route::post("password/email", [
+    ForgotPasswordController::class,
+    "sendResetLinkEmail",
+  ])->name("password.email");
 
-// Forgot Password
-Route::get("password/reset", [
-  ForgotPasswordController::class,
-  "showLinkRequestForm",
-])->name("password.request");
-Route::post("password/email", [
-  ForgotPasswordController::class,
-  "sendResetLinkEmail",
-])->name("password.email");
+  // Reset Password
+  Route::get("password/reset/{token}", [
+    ResetPasswordController::class,
+    "showResetForm",
+  ])->name("password.reset");
+  Route::post("password/reset", [
+    ResetPasswordController::class,
+    "reset",
+  ])->name("password.update");
+});
 
-// Reset Password
-Route::get("password/reset/{token}", [
-  ResetPasswordController::class,
-  "showResetForm",
-])->name("password.reset");
-Route::post("password/reset", [
-  ResetPasswordController::class,
-  "reset",
-])->name("password.update");
+
 
 Route::group(['middleware' => 'auth'], function() {
+  // logout
+  Route::post("logout", [LoginController::class, "logout"])->name("logout");
+
   Route::get('/dashboard', [UsersController::class, 'index'])->name('dashboard');
   Route::get('/profile', [UsersController::class, 'profile'])->name('profile');
   Route::get('/profile/edit', [UsersController::class, 'edit'])->name('profile.edit');
