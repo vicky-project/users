969dba790eba5c251cb97b3147115da8ca906087
vicky <pratamavr@gmail.com>
2026-03-12 06:15:14 +0800

    updates

---
+++ app/Installations/PostInstallation.php
@@ -0,0 +1,21 @@
+<?php
+namespace Modules\Users\Installations;
+
+use Nwidart\Modules\Facades\Module;
+use Illuminate\Support\Facades\Artisan;
+
+class PostInstallation {
+  public function handle(string $moduleName) {
+    try {
+      Artisan::call("vendor:publish", ["--tag" => "sanctum-migrations"]);
+      Artisan::call("migrate", ["--force" => true]);
+    } catch(\Exception $e) {
+      logger()->error(
+        "Failed to run post installation of users module: " .
+        $e->getMessage()
+      );
+
+      throw $e;
+    }
+  }
+}
