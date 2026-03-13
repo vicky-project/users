a0feb77ba21c148af559fecf8fce502792b14b92
vicky <pratamavr@gmail.com>
2026-03-14 01:06:28 +0800

    updates

--- resources/views/hooks/apps.blade.php
+++ resources/views/hooks/apps.blade.php
@@ -1,5 +1,5 @@
 <div class="col-4 col-md-2 mb-2">
-  <a href="{{ route('apps.index') }}" class="menu-item rounded-4 p-2" disabled="true">
+  <a href="{{ route('apps.index') }}" class="menu-item rounded-4 p-2 disabled" disabled="true">
     <i class="bi bi-app"></i>
     <span>Application</span>
   </a>
